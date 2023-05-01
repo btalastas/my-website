e4c7f0d6c0e15c5f0e18f071f87e1a7033985062
bjornhehe <talastasbjorn@gmail.com>
2023-04-30 23:36:56 -0400

    added header bar with a profile pic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import * as React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import HeaderBar from './components/HeaderBar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <ChakraProvider>
+    <HeaderBar></HeaderBar>
+      <div className="App">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +21,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      
     </div>
+    </ChakraProvider>
   );
 }
 
---
+++ src/components/HeaderBar.js
@@ -0,0 +1,23 @@
+import { Box } from "@chakra-ui/react";
+import { Flex, Spacer, Heading, Badge, Avatar, Text } from "@chakra-ui/react";
+export default function HeaderBar() {
+  return (
+    <Flex minWidth="max-content" alignItems="center" gap="1" padding='5' bgColor="twitter.500">
+      <Box p="2">
+        <Heading size="md">Btal Domain</Heading>
+      </Box>
+      <Spacer />
+        
+      <Avatar src="/profile_pic.png" size="2xl"/>
+      <Box ml="3">
+        <Text fontWeight="bold" outline={25}>
+          Bjorn Talastas
+          <Badge ml="1" colorScheme="green">
+            Student
+          </Badge>
+        </Text>
+        <Box as="Text" fontSize="sm" borderRadius='md' bg='white' color='black' px={1} h={10}>George Mason 2024</Box>
+      </Box>
+    </Flex>
+  );
+}
