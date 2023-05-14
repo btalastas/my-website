277b5cca11e7da8ede30648c9ef1759e03762adf
bjornhehe <talastasbjorn@gmail.com>
2023-05-14 01:20:50 -0400

    two seperate boxes in leff most header box

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -2,20 +2,32 @@ import { Box } from "@chakra-ui/react";
 import { Flex, Spacer, Heading, Badge, Avatar, Text } from "@chakra-ui/react";
 export default function HeaderBar() {
   return (
-    <Flex minWidth="max-content" alignItems="center" gap="1" padding='5' bgColor="twitter.500">
+    <Flex
+      minWidth="max-content"
+      alignItems="center"
+      gap="1"
+      padding="5"
+      bgColor="twitter.500"
+    >
       <Box p="2">
-        <Heading size="md">Btal Domain</Heading>
-        <Heading size="md"> More Stuff Here</Heading>
-        <Text height="50%">hello</Text>
+        <Box display="grid" position="absolute" top="0">
+          <Heading size="md">Btal Domain</Heading>
+          <Heading size="md"> More Stuff Here</Heading>
+        </Box>
+        <Box display="grid" position="absolute" bottom="0">
+          <Text>link 1</Text>
+          <Text>link 2</Text>
+          <Text>link 2</Text>
+        </Box>
       </Box>
       <Spacer />
 
       <Box p="2">
-        <Heading size="md" > Extra stuff here</Heading>
+        <Heading size="md"> Extra stuff here</Heading>
       </Box>
       <Spacer />
-        
-      <Avatar src="/profile_pic.png" size="2xl"/>
+
+      <Avatar src="/profile_pic.png" size="2xl" />
       <Box ml="3">
         <Text fontWeight="bold" outline={25}>
           Bjorn Talastas
@@ -23,9 +35,18 @@ export default function HeaderBar() {
             Student
           </Badge>
         </Text>
-        <Box as="Text" fontSize="sm" borderRadius='md' bg='white' color='black' px={1} h={10}>George Mason 2024</Box>
+        <Box
+          as="Text"
+          fontSize="sm"
+          borderRadius="md"
+          bg="white"
+          color="black"
+          px={1}
+          h={10}
+        >
+          George Mason 2024
+        </Box>
       </Box>
-
     </Flex>
   );
 }
